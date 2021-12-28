4ef0558ec7a519dc8efb184654cb02f3792ff1ac
norbertcserpnyak16 <norbi.cserpnyak@gmail.com>
2021-12-28 13:35:04 +0100

    Játék forráskódjának kommentelése

--- eclipse-workspace/Gyakorlás/src/Gyakorlas.java
+++ eclipse-workspace/Gyakorlás/src/Gyakorlas.java
@@ -1,29 +1,29 @@
-import java.util.*; 
+import java.util.*; //Beimportálom a Java Util csomagot
  
 public class Gyakorlas {
  
     public static void main(String[] args) {
-        System.out.println("BUÉK! :)"); 
-        boolean nyert = false; 
-        int szam = (int) (Math.random()*10)+1; 
+       // System.out.println("BUÉK! :)"); //Kiiratom, hogy Buék
+        boolean nyert = false; //boolean nyert változó false értéken van
+        int szam = (int) (Math.random()*10)+1; //int szam változó tipuskényszerítéssel math.random függvény amihez szorzunk 10-el és hozzáadok 1-et
         
-        for (int i = 0; i < 2; i++) {
-        	System.out.println("Találd ki azt a számot amire gondoltam!");
-            int tippeles = new Scanner(System.in).nextInt();
+        for (int i = 0; i < 2; i++) { //for ciklus 2 tippelési lehetőséget ad.
+        	System.out.println("Találd ki azt a számot amire gondoltam!"); //Kiiratás
+            int tippeles = new Scanner(System.in).nextInt(); //Adat bekérés a System.in-ről
   
-           if (tippeles==szam) {
-        	   nyert=true;
-   				break;
+           if (tippeles==szam) { //Ha a tipelés egyenlő a számmal
+        	   nyert=true; //nyert true értékre kapcsolása
+   				break; //kilépés a programból
         	   
            			}
 			}
        
-        	if(nyert==false) {
-			System.out.println("Nem Nyertél");
+        	if(nyert==false) { //Ha a nyert false 
+			System.out.println("Nem Nyertél"); //Kiirja, hogy nem nyertél és a számot amit ki kellett volna találni
 			System.out.println("A kitalált szám: "+szam);
         	
-        	}else {
-				System.out.println("Nyertél");
+        	}else { //Különben, ha igaz a nyert változó
+				System.out.println("Nyertél");//Irassa ki, hogy nyert.
 			}
 			
 		}   
